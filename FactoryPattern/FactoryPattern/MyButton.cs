21eb974817aab7dd9d4b48f9eca3670e9d6254d7
lunarnublet <lunarnublet@gmail.com>
2017-04-17 11:49:02 -0600

    Got rid of previous library and updated references.

--- FactoryPattern/FactoryPattern/MyButton.cs
+++ FactoryPattern/FactoryPattern/MyButton.cs
@@ -1,10 +1,10 @@
-﻿using FactoryLibrary;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using IDE;
 
 namespace FactoryPattern
 {
---
+++ FactoryPattern/IDE/AbstractFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDE
+{
+    public abstract class AbstractFactory
+    {
+        public abstract AbstractElement GetInstance(string type, int height, int width, int x, int y);
+        public abstract string GetBeginnings();
+        public abstract string GetEndings();
+    }
+}
